3cfc38461264a3b131c447980d1ff3df2931c137
lukimen <lukmanhakim174@gmail.com>
2017-12-11 01:21:03 +0700

    remove author

--- contents/js/all-script.js
+++ contents/js/all-script.js
@@ -19,7 +19,7 @@ $(window).on('load', function(){
   $('.article__content img').each(function(){
 		var firstSrc = 	$(this).attr('src');
 		if(!ROOT_URL){
-			ROOT_URL = 'https://ft-umj-4.github.io/story';
+			ROOT_URL = 'https://metlandcikini.github.io';
 		}
 	    $(this).attr('src', ROOT_URL + firstSrc);
 	});
